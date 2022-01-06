d7b1a590c22f9a4b847916189ba39cfaa25133d0
juan castro <juan.castro@geniussports.com>
2022-01-06 10:22:22 -0500

    New feature v4

--- packages/common/index.js
+++ packages/common/index.js
@@ -1,5 +1,6 @@
 module.exports = () => {
   console.log("hello from common 12");
   console.log("feature V2");
-  console.log("feature V3")
+  console.log("feature V3");
+  console.log("feature V4");
 };
